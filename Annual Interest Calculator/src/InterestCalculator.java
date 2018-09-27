494f61c9c84edca46c0055c55858dcd9aca7c287
Mitkay <36478732+timwaik@users.noreply.github.com>
2018-09-27 23:05:57 +0100

    Add files via upload
    Program will now continuously run until user quits himself.
    Added a minor feature where at the end of the program users can either pick up where they left off with their current balance, or start over from scratch.

--- Annual Interest Calculator/src/InterestCalculator.java
+++ Annual Interest Calculator/src/InterestCalculator.java
@@ -1,7 +1,6 @@
+
 /*
  * This code is functional, but does not implement any try/catch blocks,
- * Any code that has been commented out is unnecessary, but may be implemented 
- * in the future.
  * */
 
 import java.util.Scanner;
@@ -16,7 +15,7 @@ public class InterestCalculator {
 
 	public static float baseBalance() {
 		float baseBalance;
-		System.out.println("How much money do you currently have in your account?");
+		System.out.println("\nHow much money do you currently have in your account?");
 		baseBalance = input.nextFloat();
 		input.nextLine();
 		return baseBalance;
@@ -24,20 +23,14 @@ public class InterestCalculator {
 
 	public static int numberOfYears() {
 		int years;
-		System.out.println("How many years would you like to run this simulation for?");
+		System.out.println("\nHow many years would you like to run this simulation for?");
 		years = input.nextInt();
 		input.nextLine();
 		return years;
 	}
 
-	/*public static float totalBalance() {
-		float totalBalance = 0;
-		System.out.println("Your total cash now amounts to: " + totalBalance);
-		return totalBalance;
-	}*/
-
 	public static float interestRate() {
-		System.out.println("What's your annual interest rate?");
+		System.out.println("\nWhat's your annual interest rate?");
 		float interest;
 		interest = input.nextFloat();
 		input.nextLine();
@@ -48,7 +41,7 @@ public class InterestCalculator {
 	public static int fixedOrVariable() {
 		int indicator;
 		String type = null;
-		System.out.println("Are you depositing a fixed amount or a variable amount every month?");
+		System.out.println("\nAre you depositing a fixed amount or a variable amount every month?");
 		System.out.println("Type '1' for fixed or '2' for variable: ");
 		indicator = input.nextInt();
 		input.nextLine();
@@ -66,10 +59,11 @@ public class InterestCalculator {
 	public static float[] fixedMonthlyDeposit(int years) {
 		float fixedDeposit[] = new float[years];
 		for (int i = 0; i < years; i++) {
-			System.out.println("How much money are you depositing monthly for year " + (i+1));
+			System.out.println("\nHow much money are you depositing monthly for year " + (i + 1));
 			fixedDeposit[i] = input.nextFloat();
 			input.nextLine();
 		}
+		System.out.println("\nCalculating..");
 		return fixedDeposit;
 	}
 
@@ -87,18 +81,18 @@ public class InterestCalculator {
 		return variableMonthlyDeposit;
 	}
 
-	public static void annualInterestFixedIncome(float interest, float[] fixedDeposit, float baseBalance, int years) {
+	public static float annualInterestFixedIncome(float interest, float[] fixedDeposit, float baseBalance, int years) {
 		float newBalance = baseBalance;
 		float annualInterestIncome = 0;
 		float totalBalance = 0;
 		float totalMonthlyBalance = 0;
 		float monthlyInterest;
-		
+
 		int counter = 0;
-		float[] totalMonthlyBalanceArray = new float[years*12];
-		float[] accruedInterestIncomeArray = new float[years*12];
-		float[] monthlyInterestArray = new float[years*12];
-		
+		float[] totalMonthlyBalanceArray = new float[years * 12];
+		float[] accruedInterestIncomeArray = new float[years * 12];
+		float[] monthlyInterestArray = new float[years * 12];
+
 		for (int i = 1; i <= years; i++) {
 			for (int deposit = 1; deposit < 13; deposit++) {
 				newBalance += fixedDeposit[i - 1];
@@ -106,105 +100,131 @@ public class InterestCalculator {
 				monthlyInterest = ((totalBalance) * interest / 12);
 				annualInterestIncome += monthlyInterest;
 				totalMonthlyBalance = newBalance + annualInterestIncome;
-				
+
 				totalMonthlyBalanceArray[counter] = totalMonthlyBalance;
 				accruedInterestIncomeArray[counter] = annualInterestIncome;
 				monthlyInterestArray[counter] = monthlyInterest;
-				
-				System.out.println("\nYour total balance for month: " + deposit + " is: " + totalMonthlyBalanceArray[counter]);
+
+				System.out.println(
+						"\nYour total balance for month: " + deposit + " is: " + totalMonthlyBalanceArray[counter]);
 				System.out.println("Your monthly paid interest amounts to: " + monthlyInterestArray[counter]);
 				System.out.println("Your accrued interest is " + accruedInterestIncomeArray[counter]);
 				counter++;
 			}
-			System.out.println("Your annual interest accrued for the year " + i + " will be: " + annualInterestIncome);
-			System.out.println("Your annual balance accrued for the year " + i + " will be: " + totalMonthlyBalance);
+			System.out.println("\nYour annual interest accrued up to year " + i + " will be: " + annualInterestIncome);
+			System.out.println("Your annual balance accrued up to year " + i + " will be: " + totalMonthlyBalance);
 			System.out.println("");
 		}
-		/*
-		 * ***THIS COMMENTED OUT SECTION DETAILS ANOTHER METHOD TO DISPLAY THE ABOVE DATA.
-		 * IT IS COMMENTED OUT AS IT IS VISUALLY UNAPPEALING.LOOKING FOR BETTER METHODS OF
-		 * DISPLAYING DATA.***
-		 *
-		 * ***
-		 * System.out.println("\nYour total balance at the end of each month is:-");
-		System.out.println(Arrays.toString(totalMonthlyBalanceArray));
-		System.out.println("Your total paid interest every month is:- ");
-		System.out.println(Arrays.toString(monthlyInterestArray));
-		System.out.println("Your total accrued interest each month is:- ");
-		System.out.println(Arrays.toString(accruedInterestIncomeArray));
-		***
-		**/
-
+		return totalMonthlyBalance;
 	}
 
-	public static void annualInterestVariableIncome(float interest, float[] variableDeposit, float baseBalance,
+	public static float annualInterestVariableIncome(float interest, float[] variableDeposit, float baseBalance,
 			int years) {
 		float newBalance = baseBalance;
 		float annualInterestIncome = 0;
 		float totalBalance;
 		float totalMonthlyBalance = 0;
 		float monthlyInterest;
-		
+
 		int counter = 0;
-		float[] totalMonthlyBalanceArray = new float[years*12];
-		float[] accruedInterestIncomeArray = new float[years*12];
-		float[] monthlyInterestArray = new float[years*12];
-		
+		float[] totalMonthlyBalanceArray = new float[years * 12];
+		float[] accruedInterestIncomeArray = new float[years * 12];
+		float[] monthlyInterestArray = new float[years * 12];
+
 		for (int i = 1; i <= years; i++) {
 			for (int deposit = 1; deposit < 13; deposit++) {
-				newBalance += variableDeposit[((i-1)*12) + (deposit - 1)];
+				newBalance += variableDeposit[((i - 1) * 12) + (deposit - 1)];
 				totalBalance = newBalance + annualInterestIncome;
 				monthlyInterest = ((totalBalance) * interest / 12);
 				annualInterestIncome += monthlyInterest;
 				totalMonthlyBalance = newBalance + annualInterestIncome;
-				
+
 				totalMonthlyBalanceArray[counter] = totalMonthlyBalance;
 				accruedInterestIncomeArray[counter] = annualInterestIncome;
 				monthlyInterestArray[counter] = monthlyInterest;
-				
-				System.out.println("\nYour total balance for month: " + deposit + " is: " + totalMonthlyBalanceArray[counter]);
+
+				System.out.println(
+						"\nYour total balance for month: " + deposit + " is: " + totalMonthlyBalanceArray[counter]);
 				System.out.println("Your monthly paid interest amounts to: " + monthlyInterestArray[counter]);
 				System.out.println("Your accrued interest is " + accruedInterestIncomeArray[counter]);
 				counter++;
-			}			
-			System.out.println("\nYour annual interest accrued for the year " + i + " will be: " + annualInterestIncome);
-			System.out.println("Your annual balance accrued for the year " + i + " will be: " + totalMonthlyBalance);
+			}
+			System.out.println("\nYour annual interest accrued up to year " + i + " will be: " + annualInterestIncome);
+			System.out.println("Your annual balance accrued up to year " + i + " will be: " + totalMonthlyBalance);
 			System.out.println("");
 		}
+		return totalMonthlyBalance;
 	}
 
-	/*public static int continueRunning() {
+	public static int continueRunning() {
 		int indicator;
 		System.out.println("Do you want to continue running the program?");
 		System.out.println("Type in '1' for yes and '2' for no. ");
 		indicator = input.nextInt();
 		input.nextLine();
 		return indicator;
-	}*/
+	}
 
-	public static void main(String[] args) {
-		float interest;
+	public static int startOver() {
+		int startOver;
+		System.out.println("Do you want to start over, (Deletes existing data) or "
+				+ "continue where you left off? (Keeps existing data)");
+		System.out.println("Type '1' to start over or '2'' to continue where you left off:");
+		startOver = input.nextInt();
+		input.nextLine();
+		System.out.print("You chose to ");
+		if (startOver == 1) {
+			System.out.println("start over.\n");
+		}
+		if (startOver == 2) {
+			System.out.println("continue where you left off.\n");
+		}
+		return startOver;
+	}
+
+	public static float resetData() {
 		float baseBalance;
-		//float totalBalance;
-		//int continueRunning;
-		int indicator;
-		int years;
 		greeting();
 		baseBalance = baseBalance();
-		//do {
-			//totalBalance = totalBalance();
-			years = numberOfYears();
+		return baseBalance;
+	}
+
+	public static void main(String[] args) {
+		float interest = 0;
+		float baseBalance = 0;
+		int continueRunning;
+		int startOver = 0;
+		int indicator = 0;
+		int years = 0;
+
+		do {
+
+			if (startOver != 2) {
+				baseBalance = resetData();
+			}
+			else if (startOver == 2) {
+				System.out.println("From your previous run of this program. Your total balance was left at:");
+				System.out.println(baseBalance + "\n");
+			}
 			interest = interestRate();
 			indicator = fixedOrVariable();
+			years = numberOfYears();
+
 			if (indicator == 1) {
 				float fixedDeposit[] = fixedMonthlyDeposit(years);
-				annualInterestFixedIncome(interest, fixedDeposit, baseBalance, years);
+				baseBalance = annualInterestFixedIncome(interest, fixedDeposit, baseBalance, years);
 			} else if (indicator == 2) {
 				float variableMonthlyDeposit[] = variableMonthlyDeposit(years);
-				annualInterestVariableIncome(interest, variableMonthlyDeposit, baseBalance, years);
+				baseBalance = annualInterestVariableIncome(interest, variableMonthlyDeposit, baseBalance, years);
 			}
-			//continueRunning = continueRunning();
-		//} while (continueRunning == 1);
+			continueRunning = continueRunning();
+			if (continueRunning == 1) {
+				startOver = startOver();
+			}
+			else {
+				System.out.println("Thank you and goodbye!");
+			}
+		} while (continueRunning == 1);
 	}
 
 }
